105c12a5a1aaca665d8297064b7db3cc5d729050
Ayush Thakur <ayushthakur@gmail.com>
2025-08-08 18:06:23 +0530

    Photo correction

--- frontend/src/utils/avatarHelper.js
+++ frontend/src/utils/avatarHelper.js
@@ -2,9 +2,9 @@ export const getAvatarFor = (wa_id) => {
     // This function returns a specific avatar based on the contact's ID
     switch (wa_id) {
         case '919937320320': // Ravi Kumar's wa_id
-            return '/ravi-kumar.png';
+            return '/ravi-kumar.PNG';
         case '929967673820': // Neha Joshi's wa_id
-            return '/neha-joshi.png';
+            return '/neha-joshi.PNG';
         default:
             // Return a default placeholder if no match is found
             return '/avatar-placeholder.png';
