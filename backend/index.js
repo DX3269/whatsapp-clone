cbcfaeae88262f9b221e67c967a50229c85436d3
Ayush Thakur <ayushthakur@gmail.com>
2025-08-08 17:29:55 +0530

    Sever correction

--- backend/index.js
+++ backend/index.js
@@ -47,6 +47,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0',() => {
   console.log(`👂 Server is listening on http://localhost:${PORT}`);
 });
